d27bc5bc60c61ac877e17e056f3aa2dad00f29d3
xsf <xsf_uestc_ncl@163.com>
2018-10-14 21:53:41 +0800

    @test main

--- app/src/main/java/com/xsf/dev/myapplication/MainActivity.java
+++ app/src/main/java/com/xsf/dev/myapplication/MainActivity.java
@@ -2,12 +2,29 @@ package com.xsf.dev.myapplication;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
-public class MainActivity extends Activity {
+import com.xsf.dev.spiannotation.ServiceProvider;
+import com.xsf.dev.spiloader.ServiceLoader;
+
+
+@ServiceProvider(value = View.OnClickListener.class, priority = 3)
+
+public class MainActivity extends Activity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        for (final View.OnClickListener listener : ServiceLoader.load(View.OnClickListener.class)) {
+            Log.i("MainActivity", listener.toString());
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
